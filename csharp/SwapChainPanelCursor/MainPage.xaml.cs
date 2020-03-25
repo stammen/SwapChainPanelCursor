b45837e77e66c762db9de9eb5d88b5cd33c8c9dd
Dale Stammen <dalestam@microsoft.com>
2020-03-25 13:08:33 -0700

    added csharp projects

---
+++ csharp/SwapChainPanelCursor/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SwapChainPanelCursor
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Start rendering animated DirectX content on a background thread, which reduces load on the UI thread
+            // and can help keep the app responsive to user input.
+            DirectXPanel1.StartRenderLoop();
+        }
+
+        /// <summary>
+        /// Invoked when this page will no longer be displayed in a Frame.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            // Stop rendering DirectX content when it will no longer be on screen.
+            DirectXPanel1.StopRenderLoop();
+        }
+    }
+}
